2e898b7e407002d3b76f306962f7a9376f095370
FarooqHassan <farooq.hassan@esend.fi>
2021-11-20 17:52:15 +0200

    adding json api holder api with paging

--- src/model/JsonPlaceHolderApi.tsx
+++ src/model/JsonPlaceHolderApi.tsx
@@ -1,10 +1,10 @@
-import axios from "axios";
-const JSON_PLACEHOLDER_API_ENDPOINT = "https://jsonplaceholder.typicode.com/";
+import axios from 'axios';
+const JSON_PLACEHOLDER_API_ENDPOINT = `https://jsonplaceholder.typicode.com/`;
 
-export const getPhotos = async () => {
+export const getPhotos = async (pageNumber: number) => {
     try {
-        const photoList = await axios.get(JSON_PLACEHOLDER_API_ENDPOINT + 'photos');
-        return photoList.data;
+        const photoList = await axios.get(JSON_PLACEHOLDER_API_ENDPOINT + `photos?_page=${pageNumber}`);
+        return photoList;
     } catch (err) {
         console.log("get photos error:", err);
     }
