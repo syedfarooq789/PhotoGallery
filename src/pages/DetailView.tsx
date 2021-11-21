2dba7169f786ec3b2f2ee27263e14b9d9c5e3e0d
FarooqHassan <farooq.hassan@esend.fi>
2021-11-21 20:09:45 +0200

    image detail view UI fixes

--- src/pages/DetailView.tsx
+++ src/pages/DetailView.tsx
@@ -3,6 +3,7 @@ import { PhotoDetails } from '../interfaces/PhotoDetails';
 import Avatar from '@mui/material/Avatar';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const DetailView = () => {
     const { state } = useLocation();
@@ -26,14 +27,40 @@ const DetailView = () => {
     return (
         <div>
             <Button variant="contained" onClick={goBack}>Go Back</Button>
-            <Avatar
-                alt={photoDetails.title}
-                src={photoDetails.url}
-                sx={{ width: '50%', height: '50%' }}
-            />
-        </div>
+            <div style={style.photoDetailContainer}>
+                <Typography style={style.typography} variant="overline" display="block" gutterBottom>
+                    AlbumId : {photoDetails.albumId}
+                </Typography>
+                <Typography style={style.typography} variant="overline" display="block" gutterBottom>
+                    {photoDetails.title}
+                </Typography>
+                <Avatar
 
+                    alt={photoDetails.title}
+                    src={photoDetails.url}
+                    sx={style.avatar}
+                />
+            </div>
+        </div>
     )
 }
 
 export default DetailView;
+
+
+const style = {
+    photoDetailContainer: {
+        flex: 1,
+        flexDirection: 'column' as 'column',
+        alignSelf: 'center',
+        display: 'flex'
+    },
+    avatar: {
+        alignSelf: 'center',
+        width: '30%',
+        height: '30%'
+    },
+    typography: {
+        alignSelf: 'center'
+    }
+}
