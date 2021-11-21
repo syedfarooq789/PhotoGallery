72e0ebe4fbf43fb061bce60e635115cf05c27b46
FarooqHassan <farooq.hassan@esend.fi>
2021-11-21 18:33:01 +0200

    added routing for gallery app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import './App.css';
-import Gallery from "./pages/Gallery";
+import Gallery from './pages/Gallery';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import DetailView from './pages/DetailView';
 
 function App() {
   return (
     <div>
-      <h1 className="App">Photo Gallery</h1>
-      <Gallery />
+      <h1 className='App'>Photo Gallery</h1>
+      <Container><RoutingComponent /></Container>
     </div>
+  );
+}
+
+function RoutingComponent() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/gallery/:pageNumber' element={<Gallery />} />
+          <Route path='/details' element={<DetailView />} />
+          <Route
+            path='/'
+            element={<Navigate to='/gallery/:pageNumber' />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
