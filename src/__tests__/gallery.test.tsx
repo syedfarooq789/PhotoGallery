4805fbd0bce69dea2f6d0362adf3cd649ea8a7d8
FarooqHassan <farooq.hassan@esend.fi>
2021-11-22 20:19:35 +0200

    gallery page test

---
+++ src/__tests__/gallery.test.tsx
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom/extend-expect"
+import { render, RenderResult, screen, waitForElement } from "@testing-library/react";
+import Enzyme from "enzyme";
+import Gallery from "../pages/Gallery";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import {
+    BrowserRouter
+} from 'react-router-dom';
+import { shallow } from "enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe("gallery view tests", () => {
+
+    let wrapper: RenderResult;
+    beforeEach(() => {
+        wrapper = render(<BrowserRouter><Gallery /></BrowserRouter>);
+    });
+
+    it('snapshot gallery page', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+});
